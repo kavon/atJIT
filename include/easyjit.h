68f6d638e19d733d84f02774c66c22f51a6dd7d7
Juan Manuel Martinez Caamaño <jmartinezcaamao@gmail.com>
2016-03-30 15:25:44 +0200

    Highlight parameters to specialize.
    Added functions to mark parameters for specialization.
    
    void foo(int x, int y) {
        y = easy_jit_specialize_parameter_long(y);
    }
    
    Will specialize the function for a particular value of y.

--- include/easyjit.h
+++ include/easyjit.h
@@ -1,3 +1,15 @@
 static void easy_jit_enabled() {
     return;
 }
+
+static long easy_jit_specialzie_param_long(long x) {
+    return x;
+}
+
+static double easy_jit_specialzie_param_double(double x) {
+    return x;
+}
+
+static void* easy_jit_specialzie_param_ptr(void* x) {
+    return x;
+}
