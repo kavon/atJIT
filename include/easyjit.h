7ac02b0244df0d396625a7921e0033471aa7d8fd
Juan Manuel Martinez Caamaño <jmartinezcaamao@gmail.com>
2017-02-06 12:01:25 +0100

    Remove the return statements

--- include/easyjit.h
+++ include/easyjit.h
@@ -1,15 +1,5 @@
-static void easy_jit_enabled() {
-    return;
-}
+static void easy_jit_enabled() {}
 
-static long easy_jit_specialzie_param_long(long x) {
-    return x;
-}
-
-static double easy_jit_specialzie_param_double(double x) {
-    return x;
-}
-
-static void* easy_jit_specialzie_param_ptr(void* x) {
-    return x;
-}
+static void easy_jit_specialzie_param_long(long) {}
+static void easy_jit_specialzie_param_double(double) {}
+static void easy_jit_specialzie_param_ptr(void*) {}
