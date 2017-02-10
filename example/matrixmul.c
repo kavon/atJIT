18cff71fb4f6407cadc52ce88902914b79ce0034
jmmartinez <jmmartinez@quarkslab.com>
2017-02-10 10:28:28 +0100

    Tabulation on example

--- example/matrixmul.c
+++ example/matrixmul.c
@@ -14,22 +14,14 @@ typedef struct {
   unsigned size;
 } matrix;
 
-void check(long l) {
-	printf("\t%ld\n", l);
-}
-
-void check_addr(void *p) {
-	printf("\t\t%p\n", p);
-}
-
 matrix create_matrix(unsigned new_size, double val) {
   int i, j, k;
   matrix _new;
   _new.size = new_size;
-  _new.rows = malloc(sizeof(double*) * new_size);
+  _new.rows = (double*)malloc(sizeof(double*) * new_size);
 
   for (i = 0; i < new_size; ++i) {
-    _new.rows[i] = malloc(sizeof(double) * new_size);
+    _new.rows[i] = (double*)malloc(sizeof(double) * new_size);
 
     for (j = 0; j < new_size; ++j) {
       _new.rows[i][j] = val;
@@ -50,18 +42,19 @@ void delete_matrix(matrix* a) {
 }
 
 void kernel(matrix a, matrix b, matrix* out) {
-  easy_jit_enabled();
+  easy_jit_enabled(); // function marked for extraction
 
   int i, j, k;
-  int n = a.size;
+  long n = a.size;
+  easy_jit_specialzie_param_long(n); // inline this parameter
 
-    for (i = 0; i < n; ++i) {
-      for (j = 0; j < n; ++j) {
-        out->rows[i][j] = 0.0;
-        for (k = 0; k < n; ++k) {
-	  out->rows[i][j] += a.rows[i][k] * b.rows[k][j];
-        }
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      out->rows[i][j] = 0.0;
+      for (k = 0; k < n; ++k) {
+        out->rows[i][j] += a.rows[i][k] * b.rows[k][j];
       }
+    }
   }
 }
 
@@ -69,22 +62,24 @@ void verify_kernel(matrix a, matrix b, matrix* out) {
   int i, j, k;
   unsigned n = a.size;
 
-    for (i = 0; i < n; ++i) {
-      for (j = 0; j < n; ++j) {
-	double verif = 0.0;
-        for (k = 0; k < n; ++k) {
-          verif += a.rows[i][k] * b.rows[k][j];
-        }
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      double verif = 0.0;
+      for (k = 0; k < n; ++k) {
+        verif += a.rows[i][k] * b.rows[k][j];
+      }
 
-        if(fabs(out->rows[i][j] - verif) > 0.01) {
-		printf("On element i=%d j=%d    expected=%f got=%f.\n", i, j, verif, out->rows[i][j]);
-	}
+      if(fabs(out->rows[i][j] - verif) > 0.01) {
+        printf("On element i=%d j=%d    expected=%f got=%f.\n", i, j, verif, out->rows[i][j]);
       }
     }
+  }
 }
 
 int main(int argc, char** argv) {
-  matrix min_a = create_matrix(size, 3.14), min_b = create_matrix(size, 3.14), mout = create_matrix(size, 0.0);
+  matrix min_a = create_matrix(size, 3.14),
+         min_b = create_matrix(size, 3.14),
+         mout = create_matrix(size, 0.0);
 
   kernel(min_a, min_b, &mout);
 #ifndef NO_VERIF
