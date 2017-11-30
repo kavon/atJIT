afd984c5b2404c997d511504e059174329aed13f
jmmartinez <jmmartinez@quarkslab.com>
2017-11-30 18:53:10 +0100

    Enable rtti on the llvm script

--- misc/get_llvm.sh
+++ misc/get_llvm.sh
@@ -6,5 +6,5 @@ git clone https://github.com/llvm-mirror/clang.git -b release_50 llvm/tools/clan
 mkdir -p llvm/_build
 cd llvm/_build
 
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_CCACHE_BUILD=1 -DLLVM_ENABLE_ASSERTIONS=1 -DLLVM_TARGETS_TO_BUILD=X86 -DBUILD_SHARED_LIBS=1 -G Ninja 
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_ENABLE_RTTI=1 -DLLVM_CCACHE_BUILD=1 -DLLVM_ENABLE_ASSERTIONS=1 -DLLVM_TARGETS_TO_BUILD=X86 -DBUILD_SHARED_LIBS=1 -G Ninja 
 ninja 
