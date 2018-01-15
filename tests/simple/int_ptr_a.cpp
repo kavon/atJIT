af29d0ddde4a2c1cde9371e5b283b017409cf715
jmmartinez <jmmartinez@quarkslab.com>
2018-01-15 19:53:18 +0100

    int_ptr_a: improve test

--- tests/simple/int_ptr_a.cpp
+++ tests/simple/int_ptr_a.cpp
@@ -9,7 +9,10 @@
 #include <functional>
 #include <cstdio>
 
-// CHECK-IR-NOT: inttoptr
+// verify that the variable 'b' is not loaded, and the addition is performed using its constant value
+// CHECK-IR-NOT: inttoptr 
+// CHECK-IR-NOT: load i32
+// CHECK-IR: add{{.*}}4321 
 
 using namespace std::placeholders;
 
@@ -17,15 +20,15 @@ static int add (int a, int *b) {
   return a+*b;
 }
 
-int const b = 1;
+int const b = 4321;
 
 int main(int argc, char** argv) {
   easy::FunctionWrapper<int(int)> inc = easy::jit(add, _1, &b, easy::options::dump_ir(argv[1]));
 
-  // CHECK: inc(4) is 5
-  // CHECK: inc(5) is 6
-  // CHECK: inc(6) is 7
-  // CHECK: inc(7) is 8
+  // CHECK: inc(4) is 4325
+  // CHECK: inc(5) is 4326
+  // CHECK: inc(6) is 4327
+  // CHECK: inc(7) is 4328
   for(int v = 4; v != 8; ++v)
     printf("inc(%d) is %d\n", v, inc(v));
 
