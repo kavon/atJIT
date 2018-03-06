hunslater/easy-just-in-time

Serialization/Deserialization of bitcode to std::ostream/std::istream

cpplib/include/easy/runtime/BitcodeTracker.h
@@ -30,13 +30,16 @@ class BitcodeTracker {
 
   // map function to all the info required for jit compilation
   std::unordered_map<void*, FunctionInfo> Functions;
+  std::unordered_map<std::string, void*> NameToAddress;
 
   public:
 
   void registerFunction(void* FPtr, const char* Name, GlobalMapping* Globals, const char* Bitcode, size_t BitcodeLen) {
     Functions.emplace(FPtr, FunctionInfo{Name, Globals, Bitcode, BitcodeLen});
+    NameToAddress.emplace(Name, FPtr);
   }
 
+  void* getNameAddress(std::string const &Name);
   std::tuple<const char*, GlobalMapping*> getNameAndGlobalMapping(void* FPtr);
   bool hasGlobalMapping(void* FPtr) const;
 

runtime/BitcodeTracker.cpp
@@ -23,6 +23,13 @@ bool BitcodeTracker::hasGlobalMapping(void* FPtr) const {
   return InfoPtr != Functions.end();
 }
 
+void* BitcodeTracker::getNameAddress(std::string const &Name) {
+  auto Addr = NameToAddress.find(Name);
+  if(Addr == NameToAddress.end())
+    return nullptr;
+  return Addr->second;
+}
+
 std::tuple<const char*, GlobalMapping*> BitcodeTracker::getNameAndGlobalMapping(void* FPtr) {
   auto InfoPtr = Functions.find(FPtr);
   if(InfoPtr == Functions.end()) {

runtime/Function.cpp
@@ -4,7 +4,9 @@
 #include <easy/runtime/LLVMHolderImpl.h>
 #include <easy/exceptions.h>
 
-#include <llvm/Transforms/IPO/PassManagerBuilder.h> 
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
 #include <llvm/Transforms/IPO.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/Host.h> 
@@ -13,6 +15,8 @@
 #include <llvm/Analysis/TargetTransformInfo.h> 
 #include <llvm/Analysis/TargetLibraryInfo.h> 
 #include <llvm/Support/FileSystem.h>
+
+#include "Utils.h"
 
 using namespace easy;
 
@@ -90,6 +94,24 @@ static void WriteOptimizedToFile(llvm::Module const &M, std::string const& File)
   Out << M;
 }
 
+std::unique_ptr<Function>
+CompileAndWrap(const char*Name, GlobalMapping* Globals,
+               std::unique_ptr<llvm::LLVMContext> Ctx,
+               std::unique_ptr<llvm::Module> M) {
+
+  llvm::Module* MPtr = M.get();
+  std::unique_ptr<llvm::ExecutionEngine> EE = GetEngine(std::move(M), Name);
+
+  if(Globals) {
+    MapGlobals(*EE, Globals);
+  }
+
+  void *Address = (void*)EE->getFunctionAddress(Name);
+
+  std::unique_ptr<LLVMHolder> Holder(new easy::LLVMHolderImpl{std::move(EE), std::move(Ctx), MPtr});
+  return std::unique_ptr<Function>(new Function(Address, std::move(Holder)));
+}
+
 std::unique_ptr<Function> Function::Compile(void *Addr, easy::Context const& C) {
 
   auto &BT = BitcodeTracker::GetTracker();
@@ -110,12 +132,42 @@ std::unique_ptr<Function> Function::Compile(void *Addr, easy::Context const& C) 
 
   WriteOptimizedToFile(*M, C.getDebugFile());
 
-  std::unique_ptr<llvm::ExecutionEngine> EE = GetEngine(std::move(M), Name);
+  return CompileAndWrap(Name, Globals, std::move(Ctx), std::move(M));
+}
 
-  MapGlobals(*EE, Globals);
+void easy::Function::serialize(std::ostream& os) const {
+  std::string buf;
+  llvm::raw_string_ostream stream(buf);
 
-  void *Address = (void*)EE->getFunctionAddress(Name);
+  LLVMHolderImpl const *H = reinterpret_cast<LLVMHolderImpl const*>(Holder.get());
+  llvm::WriteBitcodeToFile(H->M_, stream);
+  stream.flush();
 
-  std::unique_ptr<LLVMHolder> Holder(new easy::LLVMHolderImpl{std::move(EE), std::move(Ctx)});
-  return std::unique_ptr<Function>(new Function(Address, std::move(Holder)));
+  os << buf;
+}
+
+std::unique_ptr<easy::Function> easy::Function::deserialize(std::istream& is) {
+
+  auto &BT = BitcodeTracker::GetTracker();
+
+  std::string buf(std::istreambuf_iterator<char>(is), {}); // read the entire istream
+  auto MemBuf = llvm::MemoryBuffer::getMemBuffer(llvm::StringRef(buf));
+
+  std::unique_ptr<llvm::LLVMContext> Ctx(new llvm::LLVMContext());
+  auto ModuleOrError = llvm::parseBitcodeFile(*MemBuf, *Ctx);
+  if(ModuleOrError.takeError()) {
+    return nullptr;
+  }
+
+  auto M = std::move(ModuleOrError.get());
+
+  std::string FunName = easy::GetEntryFunctionName(*M);
+
+  GlobalMapping* Globals = nullptr;
+  if(void* OrigFunPtr = BT.getNameAddress(FunName)) {
+    const char* Name;
+    std::tie(Name, Globals) = BT.getNameAndGlobalMapping(OrigFunPtr);
+  }
+
+  return CompileAndWrap(FunName.c_str(), Globals, std::move(Ctx), std::move(M));
 }

tests/simple/serialize_multifile.cpp
@@ -1,0 +1,57 @@
+// RUN: %clangxx %cxxflags %include_flags %s -DMAIN -c -o %t.main.o
+// RUN: %clangxx %cxxflags %include_flags %s -Xclang -load -Xclang %lib_pass -DLIB -c -o %t.lib.o -mllvm -easy-export="add"
+// RUN: %clangxx %ld_flags %t.main.o %t.lib.o -o %t 
+// RUN: %t > %t.out
+// RUN: %FileCheck %s < %t.out
+
+#include <easy/jit.h>
+
+#include <functional>
+#include <cstdio>
+#include <sstream>
+#include <string>
+
+using namespace std::placeholders;
+
+#ifdef LIB
+
+static int var = 0;
+
+static int add (int a, int b) {
+  return a+b+(var++);
+}
+
+std::string get_add(int b) {
+  auto inc_store = easy::jit(add, _1, 1);
+
+  std::stringstream out;
+  inc_store.serialize(out);
+  out.flush();
+
+  return out.str();
+}
+
+#endif
+
+#ifdef MAIN
+
+std::string get_add(int b);
+
+int main() {
+
+  std::string bitcode = get_add(1);
+  std::stringstream in(bitcode);
+  auto inc_load = easy::FunctionWrapper<int(int)>::deserialize(in);
+
+  // CHECK: inc(4) is 5
+  // CHECK: inc(5) is 7
+  // CHECK: inc(6) is 9
+  // CHECK: inc(7) is 11
+  for(int v = 4; v != 8; ++v) {
+    printf("inc(%d) is %d\n", v, inc_load(v));
+  }
+
+  return 0;
+}
+
+#endif

kavon/atJIT

cpplib/include/easy/runtime/BitcodeTracker.h
@@ -30,13 +30,16 @@ class BitcodeTracker {
 
   // map function to all the info required for jit compilation
   std::unordered_map<void*, FunctionInfo> Functions;
+  std::unordered_map<std::string, void*> NameToAddress;
 
   public:
 
   void registerFunction(void* FPtr, const char* Name, GlobalMapping* Globals, const char* Bitcode, size_t BitcodeLen) {
     Functions.emplace(FPtr, FunctionInfo{Name, Globals, Bitcode, BitcodeLen});
+    NameToAddress.emplace(Name, FPtr);
   }
 
+  void* getAddress(std::string const &Name);
   std::tuple<const char*, GlobalMapping*> getNameAndGlobalMapping(void* FPtr);
   bool hasGlobalMapping(void* FPtr) const;
 

runtime/BitcodeTracker.cpp
@@ -23,6 +23,13 @@ bool BitcodeTracker::hasGlobalMapping(void* FPtr) const {
   return InfoPtr != Functions.end();
 }
 
+void* BitcodeTracker::getAddress(std::string const &Name) {
+  auto Addr = NameToAddress.find(Name);
+  if(Addr == NameToAddress.end())
+    return nullptr;
+  return Addr->second;
+}
+
 std::tuple<const char*, GlobalMapping*> BitcodeTracker::getNameAndGlobalMapping(void* FPtr) {
   auto InfoPtr = Functions.find(FPtr);
   if(InfoPtr == Functions.end()) {

runtime/Function.cpp
@@ -2,9 +2,12 @@
 #include <easy/runtime/Function.h>
 #include <easy/runtime/RuntimePasses.h>
 #include <easy/runtime/LLVMHolderImpl.h>
+#include <easy/runtime/Utils.h>
 #include <easy/exceptions.h>
 
-#include <llvm/Transforms/IPO/PassManagerBuilder.h> 
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
 #include <llvm/Transforms/IPO.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/Host.h> 
@@ -13,6 +16,7 @@
 #include <llvm/Analysis/TargetTransformInfo.h> 
 #include <llvm/Analysis/TargetLibraryInfo.h> 
 #include <llvm/Support/FileSystem.h>
+
 
 using namespace easy;
 
@@ -90,6 +94,24 @@ static void WriteOptimizedToFile(llvm::Module const &M, std::string const& File)
   Out << M;
 }
 
+std::unique_ptr<Function>
+CompileAndWrap(const char*Name, GlobalMapping* Globals,
+               std::unique_ptr<llvm::LLVMContext> Ctx,
+               std::unique_ptr<llvm::Module> M) {
+
+  llvm::Module* MPtr = M.get();
+  std::unique_ptr<llvm::ExecutionEngine> EE = GetEngine(std::move(M), Name);
+
+  if(Globals) {
+    MapGlobals(*EE, Globals);
+  }
+
+  void *Address = (void*)EE->getFunctionAddress(Name);
+
+  std::unique_ptr<LLVMHolder> Holder(new easy::LLVMHolderImpl{std::move(EE), std::move(Ctx), MPtr});
+  return std::unique_ptr<Function>(new Function(Address, std::move(Holder)));
+}
+
 std::unique_ptr<Function> Function::Compile(void *Addr, easy::Context const& C) {
 
   auto &BT = BitcodeTracker::GetTracker();
@@ -110,12 +132,41 @@ std::unique_ptr<Function> Function::Compile(void *Addr, easy::Context const& C) 
 
   WriteOptimizedToFile(*M, C.getDebugFile());
 
-  std::unique_ptr<llvm::ExecutionEngine> EE = GetEngine(std::move(M), Name);
+  return CompileAndWrap(Name, Globals, std::move(Ctx), std::move(M));
+}
 
-  MapGlobals(*EE, Globals);
+void easy::Function::serialize(std::ostream& os) const {
+  std::string buf;
+  llvm::raw_string_ostream stream(buf);
 
-  void *Address = (void*)EE->getFunctionAddress(Name);
+  LLVMHolderImpl const *H = reinterpret_cast<LLVMHolderImpl const*>(Holder.get());
+  llvm::WriteBitcodeToFile(H->M_, stream);
+  stream.flush();
 
-  std::unique_ptr<LLVMHolder> Holder(new easy::LLVMHolderImpl{std::move(EE), std::move(Ctx)});
-  return std::unique_ptr<Function>(new Function(Address, std::move(Holder)));
+  os << buf;
+}
+
+std::unique_ptr<easy::Function> easy::Function::deserialize(std::istream& is) {
+
+  auto &BT = BitcodeTracker::GetTracker();
+
+  std::string buf(std::istreambuf_iterator<char>(is), {}); // read the entire istream
+  auto MemBuf = llvm::MemoryBuffer::getMemBuffer(llvm::StringRef(buf));
+
+  std::unique_ptr<llvm::LLVMContext> Ctx(new llvm::LLVMContext());
+  auto ModuleOrError = llvm::parseBitcodeFile(*MemBuf, *Ctx);
+  if(ModuleOrError.takeError()) {
+    return nullptr;
+  }
+
+  auto M = std::move(ModuleOrError.get());
+
+  std::string FunName = easy::GetEntryFunctionName(*M);
+
+  GlobalMapping* Globals = nullptr;
+  if(void* OrigFunPtr = BT.getAddress(FunName)) {
+    std::tie(std::ignore, Globals) = BT.getNameAndGlobalMapping(OrigFunPtr);
+  }
+
+  return CompileAndWrap(FunName.c_str(), Globals, std::move(Ctx), std::move(M));
 }

tests/simple/serialize_multifile.cpp
@@ -1,0 +1,57 @@
+// RUN: %clangxx %cxxflags %include_flags %s -DMAIN -c -o %t.main.o
+// RUN: %clangxx %cxxflags %include_flags %s -Xclang -load -Xclang %lib_pass -DLIB -c -o %t.lib.o -mllvm -easy-export="add"
+// RUN: %clangxx %ld_flags %t.main.o %t.lib.o -o %t 
+// RUN: %t > %t.out
+// RUN: %FileCheck %s < %t.out
+
+#include <easy/jit.h>
+
+#include <functional>
+#include <cstdio>
+#include <sstream>
+#include <string>
+
+using namespace std::placeholders;
+
+#ifdef LIB
+
+static int var = 0;
+
+static int add (int a, int b) {
+  return a+b+(var++);
+}
+
+std::string get_add(int b) {
+  auto inc_store = easy::jit(add, _1, 1);
+
+  std::ostringstream out;
+  inc_store.serialize(out);
+  out.flush();
+
+  return out.str();
+}
+
+#endif
+
+#ifdef MAIN
+
+std::string get_add(int b);
+
+int main() {
+
+  std::string bitcode = get_add(1);
+  std::istringstream in(bitcode);
+  auto inc_load = easy::FunctionWrapper<int(int)>::deserialize(in);
+
+  // CHECK: inc(4) is 5
+  // CHECK: inc(5) is 7
+  // CHECK: inc(6) is 9
+  // CHECK: inc(7) is 11
+  for(int v = 4; v != 8; ++v) {
+    printf("inc(%d) is %d\n", v, inc_load(v));
+  }
+
+  return 0;
+}
+
+#endif