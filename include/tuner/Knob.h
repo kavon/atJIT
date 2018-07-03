c0f634864968fffb48fa57f9b78efc7e1e73875f
Kavon Farvardin <kavon@farvard.in>
2018-07-03 15:42:51 -0500

    associate a general uniqueness with each knob

--- include/tuner/Knob.h
+++ include/tuner/Knob.h
@@ -8,15 +8,31 @@ namespace tuner {
 
   // polymorphism of Knobs is primarily achieved through inheritance.
 
+  using KnobID = uint64_t;
+
+namespace {
+  // used to ensure knob IDs are unique.
+  static KnobID KnobTicker = 1;
+}
+
   // Base class for tunable compiler "knobs", which
   // are simply tunable components.
   template< typename ValTy >
   class Knob {
 
+  private:
+    KnobID id;
+
   public:
+    Knob() {
+      id = KnobTicker++;
+    }
     // value accessors
     virtual ValTy getVal() const = 0;
     virtual void setVal(ValTy) = 0;
+
+    // a unique ID relative to all knobs in the program.
+    KnobID getID() const { return id; }
 
   }; // end class Knob
 
