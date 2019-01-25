83f4acd8842db5b272ea9cac8028d826b4aa65b8
Kavon Farvardin <kavon@farvard.in>
2019-01-25 14:54:09 -0600

    manually sync the llvm, clang, and polly sources to compatible versions

--- get-llvm-with-polly.sh
+++ get-llvm-with-polly.sh
@@ -25,7 +25,7 @@ else
 fi
 
 
-pushd $1
+cd $1
 
 #####
 # make sure we're in an empty dir
@@ -37,21 +37,32 @@ fi
 #####
 # get sources
 
-git clone --branch pragma --single-branch --depth 1 https://github.com/Meinersbur/llvm.git src
+# PULL LLVM
+git clone --branch pragma --single-branch https://github.com/Meinersbur/llvm.git src
+cd ./src
+git reset --hard cf50706d0b788f21f58e0ac7f9fef8c3f010a6f1
 
-pushd src/tools
+cd ./tools
 
-git clone --branch pragma --single-branch --depth 1 https://github.com/Meinersbur/clang.git
+## PULL CLANG
+git clone --branch pragma --single-branch https://github.com/Meinersbur/clang.git
+cd ./clang
+git reset --hard d0dc69f8de9250e68233ee8b56db5b2517d4aaf3
+cd ..
 
-git clone --branch pragma --single-branch --depth 1 https://github.com/Meinersbur/polly.git
+# PULL POLLY
+git clone --branch pragma --single-branch https://github.com/Meinersbur/polly.git
+cd ./polly
+git reset --hard 70baac1f268f77e976f9b0805db7ff5c14c06df2
+cd ..
 
-popd
+cd ../..
 
 ##################
 ## configure & build
 
 mkdir build install
-pushd build
+cd ./build
 cmake -C $DIR/cmake/LLVM.cmake -G "$GENERATOR" -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_INSTALL_PREFIX=../install ../src
 
 $BUILD_CMD
